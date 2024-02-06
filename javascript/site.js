b1788e4ce84907d4684c17648296b3e161ae4a0b
Aria Lind <143350774+AriaLind@users.noreply.github.com>
2024-02-06 13:53:09 +0100

    asdfasfas

--- javascript/site.js
+++ javascript/site.js
@@ -7,33 +7,42 @@ const kebabPizza = new Pizza("Kebab Pizza", 5.0);
 
 const cart = [];
 
-const cartItemName = document.getElementById('cart-item-name');
-const cartItemPrice = document.getElementById('cart-item-price');
-
-
-
 if (document.getElementById('kebabButton') !== null){
     const kebabButton = document.getElementById('kebabButton');
     kebabButton.addEventListener('click', function() {addToCart(kebabPizza)});
 }
 
-async function addToCart(product){
+const cartItemName = document.getElementById('cart-item-name');
+const cartItemPrice = document.getElementById('cart-item-price');
+const cartTotalPrice = document.getElementById('cart-total-price')
+
+async function addToCart(product) {
     cart.push(product);
     console.log("Item added to cart:", product);
+
+    let totalCost = 0;
+
     cart.forEach(item => {
-        const itemElement = document.createElement('div');
-        const nameElement = document.createElement('span');
-        const priceElement = document.createElement('span');
-    
-        nameElement.textContent = item.name;
-        priceElement.textContent = item.price.toFixed(2);
-    
-        itemElement.appendChild(nameElement);
-        itemElement.appendChild(document.createTextNode(' - $'));
-        itemElement.appendChild(priceElement);
-        itemElement.appendChild(document.createElement('br'));
-    
-        cartItemName.appendChild(itemElement);
+        totalCost += item.price;
     });
+
+    console.log(totalCost);
+
+    const itemElement = document.createElement('div');
+    const nameElement = document.createElement('span');
+    const priceElement = document.createElement('span');
+
+    nameElement.textContent = product.name;
+    priceElement.textContent = '$' + product.price.toFixed(2);
+
+    itemElement.appendChild(nameElement);
+    itemElement.appendChild(document.createElement('br'));
+    itemElement.appendChild(document.createTextNode('$'));
+    itemElement.appendChild(priceElement);
+    itemElement.appendChild(document.createElement('br'));
+
+    cartItemName.appendChild(itemElement);
+    cartTotalPrice.textContent = 'Total Cost: $' + totalCost.toFixed(2);
 }
+
 
