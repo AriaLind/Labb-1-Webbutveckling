12200c739246075e35c60d0c8ea63d8bd62ee98a
Aria Lind <143350774+AriaLind@users.noreply.github.com>
2024-02-15 19:46:52 +0100

    fdgsdgdgsf

--- javascript/site.js
+++ javascript/site.js
@@ -1,79 +1,138 @@
 let cart = [];
 let totalCost = 0;
 
-//#region Classes
 
-class Pizza{
-    constructor(name, price){
-        this.name = name;
-        this.price = price;
-    }
-}
+const Pizzas =[
+    {
+        name: "Kebab Pizza",
+        ingredients: "Kebab meat, Red onion, Bell peppers, Tomatoes, Jalapeños, Mozzarella cheese, Pizza sauce or tomato sauce, Kebab sauce or garlic sauce",
+        price: 7.99,
+        imageUrl: "../img/Kebab Pizza.webp",
+        imageAlt: "KebabPizza.webp",
+        description: "Our signature kebab pizza is a tantalizing blend of succulent kebab meat, crisp red onions, vibrant bell peppers, zesty tomatoes, and spicy jalapeños atop a rich tomato or pizza sauce base. Topped with creamy mozzarella cheese and your choice of kebab or garlic sauce, each bite bursts with flavor and texture, creating a culinary experience that's simply irresistible."
+    },
+    {
+        name: "Salami Pizza",
+        ingredients: "Salami, Mozzarella cheese, Tomato sauce or pizza sauce, Red onion, Bell peppers, Black olives (optional), Mushrooms (optional), Fresh basil (optional)",
+        price: 6.99,
+        imageUrl: "../img/Salami Pizza.webp",
+        imageAlt: "SalamiPizza.webp",
+        description: "Indulge in our classic Salami Pizza, crafted with savory salami slices, velvety mozzarella cheese, and tangy tomato or pizza sauce. Enhanced with the aromatic notes of red onions, crisp bell peppers, and optional black olives, mushrooms, or fresh basil, it's a symphony of flavors that will delight your taste buds with every bite."
+    },
+    {
+        name: "Calzone",
+        ingredients: "Salami, Ricotta cheese, Mozzarella cheese, Tomato sauce or marinara sauce, Black olives (optional), Mushrooms (optional), Fresh spinach (optional), Garlic (optional)",
+        price: 6.99,
+        imageUrl: "../img/Calzone.webp",
+        imageAlt: "Calzone.webp",
+        description: "Savor the essence of Italy with our delectable Calzone. Stuffed with premium salami, creamy ricotta cheese, and gooey mozzarella, it's a taste of Italian tradition. Enjoy the medley of flavors from zesty tomato sauce or marinara, complemented by optional black olives, mushrooms, fresh spinach, and garlic. Every bite is a journey through the rustic flavors of Italy, wrapped in a golden crust for a perfect culinary experience."
+    },
+    {
+        name: "Margherita Pizza",
+        ingredients: "Fresh basil leaves, Mozzarella cheese, Tomato sauce",
+        price: 4.99,
+        imageUrl: "../img/Margherita Pizza.webp",
+        imageAlt: "MargheritaPizza.webp",
+        description: "Indulge in the simplicity of our Margherita Pizza, a true Italian classic. Crafted with fresh basil leaves, rich mozzarella cheese, and savory tomato sauce, it embodies the essence of traditional Italian cuisine. Each bite is a harmonious blend of flavors, where the vibrant basil meets the creamy cheese atop a bed of tangy tomato sauce. Experience the authentic taste of Italy in every slice of our Margherita Pizza."
+    },
+    {
+        name: "Hawaiian Pizza",
+        ingredients: "Pineapple chunks, Ham or Canadian bacon, Mozzarella cheese, Tomato sauce",
+        price: 5.99,
+        imageUrl: "../img/Hawaiian Pizza.webp",
+        imageAlt: "HawaiianPizza.webp",
+        description: "Savor the tropical flavors of our Hawaiian Pizza, a delightful blend of sweet and savory. Featuring juicy pineapple chunks, succulent ham or Canadian bacon, and gooey mozzarella cheese atop a luscious tomato sauce base, it's a taste of paradise in every bite. Let the contrasting flavors of sweet pineapple and savory ham transport you to the sunny shores of Hawaii with our Hawaiian Pizza."
+    },
+]
 
-const kebabLarge = new Pizza("Kebab Pizza", 7.99);
-const kebabMedium = new Pizza("Kebab Pizza", 6.99);
-const kebabSmall= new Pizza("Kebab Pizza", 5.99);
+const pizzaDisplay = document.getElementById('pizza-display');
 
-const salamiLarge = new Pizza("Salami Pizza", 6.99);
-const salamiMedium = new Pizza("Salami Pizza", 5.99);
-const salamiSmall = new Pizza("Salami Pizza", 4.99);
+Pizzas.forEach(pizza => {
+    
+    const pizzaInfoDiv = document.createElement('div');
+    pizzaInfoDiv.classList.add('pizza-info');
 
-const calzoneLarge = new Pizza("Calzone", 6.99);
-const calzoneMedium = new Pizza("Calzone", 5.99);
-const calzoneSmall = new Pizza("Calzone", 4.99);
+    const pizzaTitle = document.createElement('h2');
+    pizzaTitle.textContent = pizza.name;
 
-const margheritaLarge = new Pizza("Margherita Pizza", 4.99);
-const margheritaMedium = new Pizza("Margherita Pizza", 3.99);
-const margheritaSmall = new Pizza("Margherita Pizza", 2.99);
+    const pizzaContentDiv = document.createElement('div');
+    pizzaContentDiv.classList.add('pizza-content');
 
-const hawaiianLarge = new Pizza("Hawaiian Pizza", 5.99);
-const hawaiianMedium = new Pizza("Hawaiian Pizza", 4.99);
-const hawaiianSmall = new Pizza("Hawaiian Pizza", 3.99);
+    const pizzaimage = document.createElement('img');
+    pizzaimage.src = pizza.imageUrl;
+    pizzaimage.alt = pizza.imageAlt;
+    pizzaimage.loading = 'lazy';
+    pizzaimage.classList.add('shadow');
 
-//#endregion
+    const pizzaDescription = document.createElement('p');
+    pizzaDescription.textContent = pizza.description;
+    
+    const pizzaModalButton = document.createElement('button');
+    pizzaModalButton.classList.add('btn', 'btn-dark', 'mb-2');
+    pizzaModalButton.textContent = 'Info';
+    pizzaModalButton.id = `${pizza.name}ModalButton`;
 
-//#region EventListeners
+    const pizzaOrderButton = document.createElement('button');
+    pizzaOrderButton.classList.add('btn', 'btn-dark', 'mb-2');
+    pizzaOrderButton.textContent = 'Order';
+    pizzaOrderButton.addEventListener('click', function() {addToCart(pizza)})
 
-const kebabButtonLarge = document.getElementById('kebabButtonLarge');
-kebabButtonLarge.addEventListener('click', function() {addToCart(kebabLarge)});
-const kebabButtonMedium = document.getElementById('kebabButtonMedium');
-kebabButtonMedium.addEventListener('click', function() {addToCart(kebabMedium)});
-const kebabButtonSmall = document.getElementById('kebabButtonSmall');
-kebabButtonSmall.addEventListener('click', function() {addToCart(kebabSmall)});
+    pizzaInfoDiv.appendChild(pizzaTitle);
+    pizzaInfoDiv.appendChild(pizzaContentDiv);
+    pizzaContentDiv.appendChild(pizzaimage);
+    pizzaContentDiv.appendChild(pizzaDescription);
+    pizzaContentDiv.appendChild(pizzaModalButton);
+    pizzaContentDiv.appendChild(pizzaOrderButton);
 
+    const pizzaModal = document.createElement('div');
+    pizzaModal.classList.add('modal');
 
-const salamiButtonLarge = document.getElementById('salamiButtonLarge');
-salamiButtonLarge.addEventListener('click', function() {addToCart(salamiLarge)});
-const salamiButtonMedium = document.getElementById('salamiButtonMedium');
-salamiButtonMedium.addEventListener('click', function() {addToCart(salamiMedium)});
-const salamiButtonSmall = document.getElementById('salamiButtonSmall');
-salamiButtonSmall.addEventListener('click', function() {addToCart(salamiSmall)});
+    const pizzaModalContent = document.createElement('div');
+    pizzaModalContent.classList.add('modal-content');
+    pizzaModal.appendChild(pizzaModalContent);
 
-const calzoneButtonLarge = document.getElementById('calzoneButtonLarge');
-calzoneButtonLarge.addEventListener('click', function() {addToCart(calzoneLarge)});
-const calzoneButtonMedium = document.getElementById('calzoneButtonMedium');
-calzoneButtonMedium.addEventListener('click', function() {addToCart(calzoneMedium)});
-const calzoneButtonSmall = document.getElementById('calzoneButtonSmall');
-calzoneButtonSmall.addEventListener('click', function() {addToCart(calzoneSmall)});
+    const modalSpan = document.createElement('span');
+    modalSpan.classList.add('close');
+    modalSpan.textContent = 'x';
+    pizzaModalContent.appendChild(modalSpan);
 
-const margheritaButtonLarge = document.getElementById('margheritaButtonLarge');
-margheritaButtonLarge.addEventListener('click', function() {addToCart(margheritaLarge)});
-const margheritaButtonMedium = document.getElementById('margheritaButtonMedium');
-margheritaButtonMedium.addEventListener('click', function() {addToCart(margheritaMedium)});
-const margheritaButtonSmall = document.getElementById('margheritaButtonSmall');
-margheritaButtonSmall.addEventListener('click', function() {addToCart(margheritaSmall)});
+    const pizzaModalBody = document.createElement('section');
+    pizzaModalBody.classList.add('modal-body');
+    pizzaModalContent.appendChild(pizzaModalBody);
+    
+    const ingredients = document.createElement('p');
+    ingredients.textContent = `Ingredients:\n ${pizza.ingredients}`
+    pizzaModalBody.appendChild(ingredients);
 
-const hawaiianButtonLarge = document.getElementById('hawaiianButtonLarge');
-hawaiianButtonLarge.addEventListener('click', function() {addToCart(hawaiianLarge)});
-const hawaiianButtonMedium = document.getElementById('hawaiianButtonMedium');
-hawaiianButtonMedium.addEventListener('click', function() {addToCart(hawaiianMedium)});
-const hawaiianButtonSmall = document.getElementById('hawaiianButtonSmall');
-hawaiianButtonSmall.addEventListener('click', function() {addToCart(hawaiianSmall)});
+    const pizzaPrice = document.createElement('section');
+    pizzaPrice.classList.add('modal-body');
+    const priceDisplay = document.createElement('p');
+    priceDisplay.textContent = `Price: $${pizza.price}`
+    pizzaPrice.appendChild(priceDisplay);
+    pizzaModalBody.appendChild(pizzaPrice);
+
+    pizzaModalButton.onclick = function() {
+        pizzaModal.style.display = "block";
+      }
+      
+
+      modalSpan.onclick = function() {
+        pizzaModal.style.display = "none";
+      }
+      
+      window.addEventListener('click', function(event) {
+        if (event.target == pizzaModal) {
+            pizzaModal.style.display = "none";
+        }
+    });
+
+    pizzaDisplay.appendChild(pizzaInfoDiv);
+    pizzaDisplay.appendChild(pizzaModal);
+
+});
 
 const checkOutButton = document.getElementById('checkOutButton')
 checkOutButton.addEventListener('click', function() {checkOut()})
-
-//#endregion
 
 //#region Functions
 
@@ -116,38 +175,6 @@ function checkOut(){
 
     console.log(`Checkout. Total cost: ${totalCost}`);
     console.log(`Cart: ${cart}`);
-}
-
-//#endregion
-
-//#region Modals
-
-var modal = document.getElementById("kebabModal");
-var modal = document.getElementById("salamiModal");
-var modal = document.getElementById("calzoneModal");
-var modal = document.getElementById("margheritaModal");
-var modal = document.getElementById("hawaiianModal");
-
-var btn = document.getElementById("kebabModalButton");
-var btn = document.getElementById("salamiModalButton");
-var btn = document.getElementById("calzoneModalButton");
-var btn = document.getElementById("margheritaModalButton");
-var btn = document.getElementById("hawaiianModalButton");
-
-var span = document.getElementsByClassName("close")[0];
-
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
 }
 
 //#endregion
